a4490e73037ff6b766403a236c76398d3b289916
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-13 17:51:29 +0000

    #1 Remove muted background

--- frontend/src/pages/authorisation/signup-page.tsx
+++ frontend/src/pages/authorisation/signup-page.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from "@/components/signup-form"
 
 export default function SignupPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <div className="text-4xl flex items-center gap-2 self-center font-medium">
           Portfolio Visualiser
