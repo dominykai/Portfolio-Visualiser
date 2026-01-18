1d08d2a71fd670eb2e3ed21f4aa297cf63b74f74
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-18 19:02:22 +0000

    #5 Attach user_router to main FastAPI app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from backend.src.routes.models.user_route import user_router
+
 app = FastAPI()
+
+API_PREFIX = "/api"
+app.include_router(user_router, prefix=API_PREFIX)
 
 if __name__ == "__main__":
     uvicorn.run(app)
