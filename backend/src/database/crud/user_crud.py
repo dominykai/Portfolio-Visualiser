a3e8c56c454dd74d4e67bd1e16fe67779b9c8bc0
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-18 19:01:50 +0000

    #5 Implement create user crud method

--- backend/src/database/crud/user_crud.py
+++ backend/src/database/crud/user_crud.py
@@ -3,8 +3,21 @@ from typing import Optional
 from sqlalchemy.orm import Session
 
 from backend.src.database.models.user_model import User
+from backend.src.schema.models.user_schema import UserCreate
+from backend.src.utils.auth.auth_utils import get_password_hash
 
 
 def get_db_user_by_email(db: Session, email: str) -> Optional[User]:
     """Return a user in the database by email."""
     return db.query(User).filter(User.email == email).first()
+
+def create_db_user(db: Session, new_user: UserCreate) -> User:
+    """Create a new user in the database, hashing passwords."""
+    db_user = User(
+        email=new_user.email,
+        password=get_password_hash(new_user.password),
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
