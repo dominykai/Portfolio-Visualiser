e11e362b1f978d766d22c3e1b0920eb48bdceca5
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-19 20:43:42 +0000

    #7 Implement AbstractBroker class with inheritable __init__ and send_request method

---
+++ backend/src/services/external/brokers/abstract_broker.py
@@ -0,0 +1,40 @@
+from abc import ABC
+from requests import get, HTTPError
+from starlette.exceptions import HTTPException
+
+
+class AbstractBroker(ABC):
+    BASE_URL: str = None
+
+    def __init__(self, api_key: str, private_key: str):
+        if self.BASE_URL is None:
+            raise NotImplementedError(self.__class__.__name__ + " does not have a BASE_URL set")
+
+        self.api_key = api_key
+        self.private_key = private_key
+
+    def send_request(self, method: str, endpoint: str, params: dict = None) -> dict:
+        """Send a request to a given API endpoint.
+
+        :param method: The HTTP method to use.
+        :param endpoint: The API Endpoint path.
+        :param params: Optional request parameters.
+        :return: Response object as a dict.
+        """
+        url = self.BASE_URL + endpoint
+        method = method.upper()
+
+        response = {}
+        try:
+            if method == "GET":
+                response = get(url=url, params=params, auth=(self.api_key, self.private_key))
+
+            response.raise_for_status()
+
+        except HTTPError:
+            # Wrap requests.HTTPError to FastAPI.HTTPException
+            raise HTTPException(
+                status_code=response.status_code,
+            )
+
+        return response.json()
