dfaeead6d1d607911af587f1105188659d872397
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-26 17:51:16 +0000

    #9 Implement symmetric encryption and decryption methods with Fernet

--- backend/src/utils/auth/auth_utils.py
+++ backend/src/utils/auth/auth_utils.py
@@ -1,4 +1,7 @@
 from bcrypt import hashpw, gensalt, checkpw
+from cryptography.fernet import Fernet
+
+from backend.src.core.config_loader import settings
 
 
 def get_password_hash(password: str) -> str:
@@ -11,3 +14,14 @@ def verify_password_hash(plain_password: str, hashed_password: str) -> bool:
         return checkpw(plain_password.encode("utf-8"), hashed_password.encode("utf-8"))
     except ValueError:
         return False
+
+def encrypt_api_key(data: str) -> str:
+    """Encrypt an API key using Fernet Encryption."""
+    return Fernet(settings.SYMMETRIC_ENCRYPTION_KEY).encrypt(data.encode("utf-8")).decode("utf-8")
+
+def decrypt_api_key(data: str) -> str:
+    """Decrypt an API key using Fernet Decryption."""
+    return Fernet(settings.SYMMETRIC_ENCRYPTION_KEY).decrypt(data.encode("utf-8")).decode("utf-8")
+
+if __name__ == "__main__":
+    print("Generated Fernet key: ", Fernet.generate_key().decode("utf-8"))
