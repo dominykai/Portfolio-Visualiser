6885beef59e7f64b6a1a36e6c4e75a0d32a92888
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-26 17:51:48 +0000

    #9 Attach api_key_router to FastAPI main app

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from backend.src.routes.auth.auth_route import auth_router
+from backend.src.routes.models.api_key_route import api_key_router
 from backend.src.routes.models.user_route import user_router
 
 app = FastAPI()
@@ -9,6 +10,7 @@ app = FastAPI()
 API_PREFIX = "/api"
 app.include_router(user_router, prefix=API_PREFIX)
 app.include_router(auth_router, prefix=API_PREFIX)
+app.include_router(api_key_router, prefix=API_PREFIX)
 
 if __name__ == "__main__":
     uvicorn.run(app)
