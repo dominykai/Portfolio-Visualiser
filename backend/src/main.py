65169d6e7d90589043ec5d9f6996e4aa6bd7a117
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-30 16:34:37 +0000

    #9 Use lifespan to preload brokers table before fastapi app startup

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,31 @@
 from contextlib import asynccontextmanager
 
+import logging
 import uvicorn
 from fastapi import FastAPI
 
+from backend.src.database.crud.broker_crud import preload_db_brokers_table_from_csv
+from backend.src.database.db import get_db
 from backend.src.routes.auth.auth_route import auth_router
 from backend.src.routes.models.api_key_route import api_key_router
 from backend.src.routes.models.user_route import user_router
 
+logging.basicConfig(level=logging.INFO)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Run before program termination
+    logging.info("Starting Lifespan")
+
+    db_session = next(get_db()) # No access to dependency injection yet
+
+    preload_db_brokers_table_from_csv(db_session)
+    logging.info("Preloaded Brokers table from CSV file")
 
     yield
     # Run after program termination
 
+    logging.info("Stopping Lifespan, Goodbye!")
 app = FastAPI(lifespan=lifespan)
 
 API_PREFIX = "/api"
