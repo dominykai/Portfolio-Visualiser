ef6401909917f6b466daefed2a6b1223e4851633
Dominic <253946929+dominykai@users.noreply.github.com>
2026-01-30 16:24:31 +0000

    #9 Add lifespan events to FastAPI app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -5,7 +7,14 @@ from backend.src.routes.auth.auth_route import auth_router
 from backend.src.routes.models.api_key_route import api_key_router
 from backend.src.routes.models.user_route import user_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Run before program termination
+
+    yield
+    # Run after program termination
+
+app = FastAPI(lifespan=lifespan)
 
 API_PREFIX = "/api"
 app.include_router(user_router, prefix=API_PREFIX)
