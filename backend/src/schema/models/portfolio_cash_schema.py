6da921b1888d8fe6d3243f2051e857e8f77b5390
Dominic <253946929+dominykai@users.noreply.github.com>
2026-02-03 00:07:12 +0000

    #10 Create a PortfolioCash schema for a pydantic model on storing portfolio cash information

---
+++ backend/src/schema/models/portfolio_cash_schema.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class PortfolioCash(BaseModel):
+    current_value: float # Current value of all investments
+    realised_profit_loss: float # The all-time realised profit loss from all the trades executed
+    total_cost: float # The cost basis of your current investments
+    unrealised_profit_loss: float # The potential profit loss of all your current investments
+    total_value: float # Investments value
